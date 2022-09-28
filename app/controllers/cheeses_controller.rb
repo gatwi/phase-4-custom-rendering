35866521bab49af5c51cf81561381244619a3998
gatwi <faith.murithi@student.moringaschool.com>
2022-09-28 08:54:57 +0300

    using custom serialization options & unique errors

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,43 @@
 class CheesesController < ApplicationController
 
-  # GET /cheeses
+  # GET /cheeses using the only: serialization option
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+  end
+
+  # GET /cheeses using the except: serialization option
+  def index
+    cheeses = Cheese.all
+    render json: cheeses, except: [:created_at, :updated_at]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    render json: {
+      id: cheese.id,
+      name: cheese.name,
+      price: cheese.price,
+      is_best_seller: cheese.is_best_seller
+    }
+  end
+
+  # GET /cheese using the methods option
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  end
+
+  # Using the nil false-y value to write our own error messaging
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      # status: :will produce a 404 code
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
   end
 
 end
